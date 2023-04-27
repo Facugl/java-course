fa86ba81eb6f42a02ceb51d5f376e5d263250292
Facundo Luna <facundolunaok@gmail.com>
2023-04-27 15:39:38 -0300

    first commit

---
+++ JavaCourse/src/main/java/com/mycompany/javacourse/JavaCourse.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.javacourse;
+
+/**
+ *
+ * @author Facundo
+ */
+public class JavaCourse {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
