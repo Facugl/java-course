eadb30fec14ceac28b7ce97c97fce2ff05b57cf6
Facundo Luna <facundolunaok@gmail.com>
2023-04-28 15:53:08 -0300

    Optimizando Ejercicio-1 mediante funciones

--- JavaCourse/src/main/java/com/mycompany/javacourse/ejercicio1/MostrarMayorYMenor.java
+++ JavaCourse/src/main/java/com/mycompany/javacourse/ejercicio1/MostrarMayorYMenor.java
@@ -12,41 +12,51 @@ import java.io.InputStreamReader;
  *
  * @author Facundo
  */
-
 // Arma un programa que permita cargar 3 números y mostrar cuál es el mayor y cual es el menor
 public class MostrarMayorYMenor {
+
     public static void main(String[] args) throws IOException {
-            InputStreamReader capturarTeclado = new InputStreamReader(System.in);
-            BufferedReader buffer = new BufferedReader(capturarTeclado);
-            
-            System.out.println("Inrese el primer número");
-            String strNumero1 = buffer.readLine();
-            Integer numero1 = Integer.parseInt(strNumero1);
-            
-            System.out.println("Inrese el segundo número");
-            String strNumero2 = buffer.readLine();
-            Integer numero2 = Integer.parseInt(strNumero2);
-            
-            System.out.println("Inrese el tercero número");
-            String strNumero3 = buffer.readLine();
-            Integer numero3 = Integer.parseInt(strNumero3);
-            
-            Integer numeroMayor = numero1; // por defecto el primero será el mayor
-            if (numero2 > numeroMayor) {
-                numeroMayor = numero2;
-            }
-            if (numero3 > numeroMayor) {
-                numeroMayor = numero3;
-            }
-            System.out.println("El número mayor es: " + numeroMayor);
-            
-            Integer numeroMenor = numero1; // por defecto el primero será el mayor          
-            if (numero2 < numeroMenor) {
-                numeroMenor = numero2;
-            }
-            if (numero3 < numeroMenor) {
-                numeroMenor = numero3;
-            }    
-            System.out.println("El número menor es: " + numeroMenor);
+        System.out.println("Inrese el primer número");
+        Integer numero1 = cargarNumero();
+
+        System.out.println("Inrese el segundo número");
+        Integer numero2 = cargarNumero();
+
+        System.out.println("Inrese el tercero número");
+        Integer numero3 = cargarNumero();
+
+        // acá llamo a mi función
+        calcularNumeroMayor(numero1, numero2, numero3);
+        calcularNumeroMenor(numero1, numero2, numero3);
+    }
+
+    private static Integer cargarNumero() throws IOException {
+        InputStreamReader capturarTeclado = new InputStreamReader(System.in);
+        BufferedReader buffer = new BufferedReader(capturarTeclado);
+        String strNumero = buffer.readLine();
+        Integer numero = Integer.parseInt(strNumero);
+        return numero;
+    }
+
+    private static void calcularNumeroMayor(Integer numero1, Integer numero2, Integer numero3) {
+        Integer numeroMayor = numero1; // por defecto el primero será el mayor
+        if (numero2 > numeroMayor) {
+            numeroMayor = numero2;
         }
+        if (numero3 > numeroMayor) {
+            numeroMayor = numero3;
+        }
+        System.out.println("El número mayor es: " + numeroMayor);
+    }
+
+    private static void calcularNumeroMenor(Integer numero1, Integer numero2, Integer numero3) {
+        Integer numeroMenor = numero1; // por defecto el primero será el menor          
+        if (numero2 < numeroMenor) {
+            numeroMenor = numero2;
+        }
+        if (numero3 < numeroMenor) {
+            numeroMenor = numero3;
+        }
+        System.out.println("El número menor es: " + numeroMenor);
+    }
 }
