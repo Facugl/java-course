52b50a82181044d6912cd1572ba116500583c605
Facundo Luna <facundolunaok@gmail.com>
2023-04-29 20:14:38 -0300

    Ejercicio 5: Sistema de Gestión de Clientes

---
+++ JavaCourse/src/main/java/EjercicioGestionClientes/Main.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package EjercicioGestionClientes;
+
+/**
+ *
+ * @author Facundo
+ */
+public class Main {
+    public static void main(String[] args) {
+        Formulario ventana = new Formulario();
+        ventana.show();
+    }
+}
